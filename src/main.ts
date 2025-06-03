b0f57513be018e89d2ad7de7dc7dbe547b9c6ba2
jsw <develjsw@gmail.com>
2025-06-03 11:13:29 +0900

    [config] config 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  const configService: ConfigService = app.get(ConfigService);
+
+  await app.listen(+configService.get('SERVER_PORT'));
 }
 bootstrap();
